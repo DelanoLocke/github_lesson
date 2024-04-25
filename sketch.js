e421ffea51fcf27910dc5fd71e0fe183db020ca1
Delano Locke <sheeplord333@outlook.com>
2024-04-25 17:06:54 +0000

    Made a circle wave

--- sketch.js
+++ sketch.js
@@ -1,40 +1,31 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(windowWidth, windowHeight)
+
+  angleMode(DEGREES)
+  rectMode(CENTER)
+}
+
+function draw() {
+  background(10,20,30)
+  noFill()
+  stroke(255)
+
+  translate(width / 2, height / 2)
   
-  function draw() {
-    background(220);
-    background("red")
-    strokeWeight(5)
-    fill('white')
-    ellipse(175, 300, 50, 30)
-    ellipse(225, 300, 50, 30)
-    fill('yellow')
-    ellipse(165, 250, 30, 50)
-    ellipse(235, 250, 30, 50)
-    rect(160, 210, 80, 80)
+  for (var i = 0; i <200; i++) {
+    push()
     
+      rotate(sin(frameCount + i*2) * 100)
     
+    var r = map(sin(frameCount), -1, 1, 50, 255)
+    var g = map(cos(frameCount / 2), -1, 1, 50, 255)
+    var b = map(sin(frameCount / 4), -1, 1, 50, 255)
     
-    triangle(165, 220, 200, 230, 185, 240)
-    triangle(235, 220, 200, 230, 215, 240)
-    fill('white')
-    strokeWeight(3)
-    ellipse(200, 242, 10, 10)
-    strokeWeight(5)
-    ellipse(170, 115, 45, 80)
-    ellipse(230, 115, 45, 80)
-    ellipse(200, 180, 120, 100)
-    strokeWeight(0)
-  
-    ellipse(175, 140, 35, 25)
-    ellipse(225, 140, 35, 25)
-    fill('black')
-    ellipse(180, 185, 10, 10)
-    ellipse(220, 185, 10, 10)
-    strokeWeight(5)
-    line(195, 200, 205, 205);
-    line(195, 205, 205, 200);
+    stroke(r,g,b)
     
-    
+    rect(0,0,600 - i * 3,600 - i *3,200 - i)
+    pop()
   }
+
+ 
+}
